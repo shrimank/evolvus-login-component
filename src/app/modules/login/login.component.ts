f09e27891d49b9f038fecc3ff09021601869d62e
shrimank <shrimankumbar@gmail.com>
2018-02-14 18:04:29 +0530

    Event Emitter added onSubmit

--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit,Input} from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,11 +8,23 @@ import { Component, OnInit,Input} from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-
+  @Output() onLogin = new EventEmitter<{username,password}>();
   @Input() title: string;
-  constructor() { }
+  loginForm: FormGroup;
+
+
+  constructor() {
+    this.loginForm = new FormGroup({
+      'username': new FormControl(null, Validators.required),
+      'password': new FormControl(null, Validators.required)
+    })
+  }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    this.onLogin.emit({username:this.loginForm.value.username,password:this.loginForm.value.password});
   }
 
 }
